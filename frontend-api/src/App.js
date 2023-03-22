a0b819bad7e06d61a5d80aaea63057534061b4d4
Daniel Theuri <dantheuri17@gmail.com>
2023-03-22 14:59:49 +0300

    added antd module. it is working

--- frontend-api/src/App.js
+++ frontend-api/src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import { DatePicker, Space } from "antd";
+import "antd/dist/reset.css";
+import "./App.css";
+
+const onChange = (date, dateString) => {
+	console.log(date, dateString);
+};
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<Space direction="vertical">
+			<DatePicker onChange={onChange} />
+			<DatePicker onChange={onChange} picker="week" />
+			<DatePicker onChange={onChange} picker="month" />
+			<DatePicker onChange={onChange} picker="quarter" />
+			<DatePicker onChange={onChange} picker="year" />
+		</Space>
+	);
 }
 
 export default App;
---
+++ frontend-api/src/Pages/EmployeeProfile.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const EmployeeProfile = () => {
+  return (
+    <div>
+        <form action="">
+            <label>
+                name:
+                <input type="text" name="" id="" />
+            
+            </label>
+
+        </form>
+    </div>
+  )
+}
+
+export default EmployeeProfile
