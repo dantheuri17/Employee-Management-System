742a918f2b220856383340ede99bf1d84c089f35
AlexisObel <69302482+AlexisObel@users.noreply.github.com>
2023-04-07 22:35:26 +0300

    Update Footer.jsx

--- frontend-api/src/Components/Footer.jsx
+++ frontend-api/src/Components/Footer.jsx
@@ -1,9 +1,21 @@
-
+import React, { useState, useEffect } from 'react';
 function Footer() {
+
+  const [employeeData, setEmployeeData] = useState([]);
+
+  useEffect(() => {
+    fetch('http://localhost:4000/employees')
+      .then(response => response.json())
+      .then(data => setEmployeeData(data))
+      .catch(error => console.error(error));
+  }, []);
+
     const footerStyle = {
         textAlign: 'center',
         color: 'white',
         height: 64,
+        bottom: 0,
+        width:'100%',
         paddingInline: 50,
         lineHeight: '64px',
         backgroundColor: '#4169E1',
@@ -12,7 +24,7 @@ function Footer() {
 
     return (
       <footer style={footerStyle}>
-                Dashboard ©2023 Created by Alexis
+                APP4035 SPRING ©2023 Group1
       </footer>
     );
   }
