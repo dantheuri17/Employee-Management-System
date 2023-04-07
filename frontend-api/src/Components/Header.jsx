3f4757ba7e17127f1a2958b483f7038c0396b1f1
AlexisObel <69302482+AlexisObel@users.noreply.github.com>
2023-04-07 22:36:02 +0300

    Update Header.jsx

--- frontend-api/src/Components/Header.jsx
+++ frontend-api/src/Components/Header.jsx
@@ -1,13 +1,19 @@
-
+import React, { useState, useEffect } from 'react';
 function Header() {
-    const headerStyle = {
-        textAlign: 'center',
-        color: '#fff',
-        height: 64,
-        paddingInline: 50,
-        lineHeight: '64px',
-        backgroundColor: '#4169E1',
-      };
+  
+  const headerStyle = {
+    textAlign: 'center',
+    color: '#fff',
+    height: 64,
+    padding: '0 50px',
+    lineHeight: '70px',
+    marginTop: 0,
+    backgroundColor: '#4169E1',
+    position: 'fixed',
+    top: 0,
+    left: 0,
+    right: 0,
+  };
 
     return (
       <header style={headerStyle}>
