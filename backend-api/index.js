1bbca33db1e999a0d827800364273440739aaf4a
AlexisObel <69302482+AlexisObel@users.noreply.github.com>
2023-04-07 22:16:48 +0300

    Update index.js

--- backend-api/index.js
+++ backend-api/index.js
@@ -1,34 +1,17 @@
-const express = require("express");
-const fs = require("fs");
+const cors = require('cors');
+const express = require('express');
 const app = express();
-const cors = require("cors");
-const port = 4000;
-const jsonParser = express.json();
-const fileName = "employees.json";
+const employees = require('./employees.json');
 
 // Allow requests only from this client
-app.use(
-	cors({
-		origin: "http://localhost:3000",
-	})
-);
+app.use(cors({
+    origin: 'http://localhost:3000'
+}));
 
-// Load data from file
-let rawData = fs.readFileSync(fileName);
-let data = JSON.parse(rawData);
-
-// This is a RESTful GET web service
-app.get("/employees", (request, response) => {
-	data.sort((a, b) => (a.firstName > b.firstName ? 1 : -1));
-	response.send(data);
+app.get('/employees', (req, res) => {
+  res.json(employees);
 });
 
-// This is a RESTful POST web service
-app.post("/employees", jsonParser, (request, response) => {
-	data.push(request.body);
-	fs.writeFileSync(fileName, JSON.stringify(data, null, 2));
-	response.end();
+app.listen(4000, () => {
+  console.log('Server started on port 4000');
 });
-
-app.listen(port);
-console.log(`server listening on port ${port}`);
