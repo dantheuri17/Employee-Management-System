d9eeb1a92020c53e11e68e79c2383b45cfb9ea4c
AlexisObel <69302482+AlexisObel@users.noreply.github.com>
2023-04-07 22:29:29 +0300

    Update Home.jsx

--- frontend-api/src/Components/Pages/Home.jsx
+++ frontend-api/src/Components/Pages/Home.jsx
@@ -1,14 +1,25 @@
 import React from 'react';
-import EmployeeStatusTable from '../EmployeeTable';
+import { Layout} from 'antd';
+import { useState, useEffect } from 'react';
+import EmployeeTable from '../EmployeeTable';
 import EmployeeDataCard from '../EmployeeDataCard';
-import BarGraph from '../LineGraph';
+import LineGraph from '../LineGraph';
 import Header from '../Header';
 import EmployeePie from '../EmployeePie';
 import Footer from '../Footer';
 
 
 function Home() {
-    
+
+  const [employeeData, setEmployeeData] = useState([]);
+
+  useEffect(() => {
+    fetch('http://localhost:4000/employees')
+      .then(response => response.json())
+      .then(data => setEmployeeData(data))
+      .catch(error => console.error(error));
+  }, []);
+
     const employeeTableStyles  = {
         position: 'absolute',
         top:'550px',
@@ -16,31 +27,31 @@ function Home() {
         left: 0,
         
       };
-    
+
     return (
-        <>
-        <div>
-          <Header/>
-        </div>
-        <div style={employeeTableStyles }>
-        <EmployeeStatusTable/>
-          </div>
-          <div>
-        <BarGraph/>
-          </div>
-          <div>
-        <EmployeeDataCard/>
-          </div>
-          <div>
-            <EmployeePie/>
-          </div>
-          <div>
-            <Footer/>
-          </div>
-            
-        </>
-      );
-    };
-  
+        
+
+
+<div style={{ overflowX: 'hidden' , overflowY: 'hidden' }}>
+   <div>
+      <Header/>
+   </div>
+   <div>
+     <EmployeeDataCard/>
+   </div>
+   <div>
+     <LineGraph/>
+   </div>
+   <div style={employeeTableStyles }>
+     <EmployeeTable/>
+   </div>
+    <div>
+    <EmployeePie/>
+    </div>
+    <Footer/>    
+           </div>
+         );
+       };
+
 
 export default Home;
