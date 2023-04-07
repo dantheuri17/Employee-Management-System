17ed5f977fc189eb28122d41cc0cf9e3a7e6cc34
AlexisObel <69302482+AlexisObel@users.noreply.github.com>
2023-04-07 22:33:37 +0300

    Update EmployeePie.jsx

--- frontend-api/src/Components/EmployeePie.jsx
+++ frontend-api/src/Components/EmployeePie.jsx
@@ -1,62 +1,75 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Pie } from '@ant-design/plots';
 import { Card } from 'antd';
-import employeeData from '../employees.json';
+
+
+
 function EmployeePie() {
+
+const [employeeData, setEmployeeData] = useState([]);
+
+useEffect(() => {
+fetch('http://localhost:4000/employees')
+.then(response => response.json())
+.then(data => setEmployeeData(data))
+.catch(error => console.error(error));
+}, []);
+
   const workTypeCounts = {}; 
   
   employeeData.forEach(employee => {
-  const workType = employee.workType;
-  if (workTypeCounts[workType]) {
-  workTypeCounts[workType]++;
-  } else {
-  workTypeCounts[workType] = 1;
-  }
+        const workType = employee.workType;
+         if (workTypeCounts[workType]) {
+          workTypeCounts[workType]++;
+          } else {
+           workTypeCounts[workType] = 1;
+         }
   });
   
   const data = Object.entries(workTypeCounts).map(([type, value]) => ({ type, value }));
   
-  const config = {
-  appendPadding: 10,
-  data,
-  angleField: 'value',
-  colorField: 'type',
-  radius: 1,
-  innerRadius: 0.6,
-  label: {
-  type: 'inner',
-  offset: '-50%',
-  content: '{value}',
-  style: {
-  textAlign: 'center',
-  fontSize: 14,
-  },
-  },
-  interactions: [
-  {
-  type: 'element-selected',
-  },
-  {
-  type: 'element-active',
-  },
-  ],
-  statistic: {
-  title: false,
-  content: {
-  style: {
-  whiteSpace: 'pre-wrap',
-  overflow: 'hidden',
-  textOverflow: 'ellipsis',
-  },
-  content: 'Work Format',
-  },
-  },
-  color: ['#1890ff', '#FF4500', '#722ed1'],
-  };
+  const config = { 
+               
+       appendPadding: 10,
+       data,
+       angleField: 'value',
+       colorField: 'type',
+       radius: 1,
+       innerRadius: 0.6,
+       label: {
+       type: 'inner',
+       offset: '-50%',
+       content: '{value}',
+       style: {
+       textAlign: 'center',
+       fontSize: 14,
+       },
+     },
+     interactions: [
+     {
+     type: 'element-selected',
+     },
+     {
+     type: 'element-active',
+     },
+      ],
+     statistic: {
+     title: false,
+     content: {
+     style: {
+     whiteSpace: 'pre-wrap',
+     overflow: 'hidden',
+     textOverflow: 'ellipsis',
+     },
+      content: 'Work Format',
+      },
+      },
+      color: ['#1890ff', '#FF4500', '#722ed1'],
+    };
   
   return (
   <Card
-  title="Propotions of Employees by work type"
+  title={<h3 style={{ fontWeight: 'bold' }}>Proportions of Employees by Work Type</h3>}
   style={{ height: '420px', width: '500px', marginTop: '40px', marginLeft: '760px'}}
   >
   <Pie
