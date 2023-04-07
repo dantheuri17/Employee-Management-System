a2dfcaf2fd5b8263ececea7dd37978ea46bc6c8e
AlexisObel <69302482+AlexisObel@users.noreply.github.com>
2023-04-07 22:37:03 +0300

    Update LineGraph.jsx

--- frontend-api/src/Components/LineGraph.jsx
+++ frontend-api/src/Components/LineGraph.jsx
@@ -1,33 +1,40 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Card} from 'antd';
 import { Line } from '@ant-design/charts';
-import employeeData from '../employees.json';
-
-const departmentCounts = {}; // object to store department counts
-
-employeeData.forEach(employee => {
-  const department = employee.department;
-  if (departmentCounts[department]) {
-    departmentCounts[department]++;
-  } else {
-    departmentCounts[department] = 1;
-  }
-});
-
-const lineConfig = {
-  data: Object.entries(departmentCounts).map(([department, count]) => ({ department, count })),
-  xField: 'department',
-  yField: 'count',
-  seriesField: null,
-  point: { size: 3, shape: 'circle' },
-  lineStyle: { lineWidth: 4 },
-  color: '#FF4500'
- 
-};
 
 const LineGraph = () => {
+  const [employeeData, setEmployeeData] = useState([]);
+
+  useEffect(() => {
+    fetch('http://localhost:4000/employees')
+      .then(response => response.json())
+      .then(data => setEmployeeData(data))
+      .catch(error => console.error(error));
+  }, []);
+
+  const departmentCounts = {}; // object to store department counts
+
+  employeeData.forEach(employee => {
+    const department = employee.department;
+    if (departmentCounts[department]) {
+      departmentCounts[department]++;
+    } else {
+      departmentCounts[department] = 1;
+    }
+  });
+
+  const lineConfig = {
+    data: Object.entries(departmentCounts).map(([department, count]) => ({ department, count })),
+    xField: 'department',
+    yField: 'count',
+    seriesField: null,
+    point: { size: 3, shape: 'circle' },
+    lineStyle: { lineWidth: 4 },
+    color: '#FF4500'
+  };
+
   return (
-    <div className="line-graph-container" style={{ position: 'absolute', top: '-343px', left: '120px', right: '0', display: 'flex', flexDirection: 'column', alignItems: 'flex-end', padding: '470px', width: '50%' }}>
+    <div className="line-graph-container" style={{ position: 'absolute', top: '-356px', left: '120px', right: '0', display: 'flex', flexDirection: 'column', alignItems: 'flex-end', padding: '470px', width: '50%' }}>
       <div className="line-graph-header" style={{ textAlign: 'right' }}>
       </div>
       <Card size="small" className="line-graph-card" style={{ width: '100%' }}>
