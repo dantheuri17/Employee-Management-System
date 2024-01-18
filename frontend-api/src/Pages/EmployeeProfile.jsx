8ac103145b920d3c0fd24770b2d50c613b44e972
Daniel Theuri <101451951+dantheuri17@users.noreply.github.com>
2024-01-18 17:32:07 +0300

    Update EmployeeProfile.jsx

--- frontend-api/src/Pages/EmployeeProfile.jsx
+++ frontend-api/src/Pages/EmployeeProfile.jsx
@@ -40,7 +40,7 @@ const EmployeeProfile = () => {
 	const [formstate, setFormstate] = useState(false);
 	const [editData, setEditData] = useState(false);
 
-	const serverHost = "employee-management-system-pink.vercel.app";
+	const serverHost = "https://employee-management-system-backend-qspc.onrender.com";
 
 	const fetchData = async () => {
 		try {
